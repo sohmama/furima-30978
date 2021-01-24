44e46b21af1fffb3e4a291dd3edcf9020363e9bf
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-01-24 16:27:34 +0900

    フロント実装の準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presemce: true
-  validates :first_name_furigana, presence: true
-  validates :last_name_furigana, presence: true
-  validates :date_of_birth, presence: true 
+  
 end
