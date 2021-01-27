151a003bf2d25afe76c75da6354a850aa8c7a07c
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-01-27 12:13:24 +0900

    マイグレーションエラーの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_24_124052) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "first_name_furigana", null: false
+    t.string "first_name", null: false
     t.string "last_name_furigana", null: false
+    t.string "first_name_furigana", null: false
     t.date "date_of_birth", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
