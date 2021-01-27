a938ac690c64c5a649e76fa56423268cc0a708d8
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-01-27 22:00:43 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presemce: true
+  validates :first_name_furigana, presence: true
+  validates :last_name_furigana, presence: true
+  validates :date_of_birth, presence: true      
+
 end
