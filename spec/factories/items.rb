ad8aba6033bf0f84439914483aac6dd1a010d716
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-02-04 22:46:38 +0900

    コメントアウト消し忘れ削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :item do
     name {Faker::Lorem.sentence}
     introduction {Faker::Lorem.sentence}
-    category_id {2} #{Category.all.sample}
-    condition_id {2} #{Condition.all.sample}
-    delivery_cost_id {2} #{DeliveryCost.all.sample}
-    prefecture_id {2} #{Prefecture.all.sample}
-    delivery_days_id {2} #{DeliveryDays.all.sample}
+    category_id {2} 
+    condition_id {2} 
+    delivery_cost_id {2} 
+    prefecture_id {2} 
+    delivery_days_id {2} 
     price {Faker::Number.between(from: 300, to: 9999999)}
     association :user
 
