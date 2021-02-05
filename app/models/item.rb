b931529f9a095f60cda9586d3486fbcd0683be6c
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-02-05 19:11:55 +0900

    コードレビューに基づく修正4

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,11 +15,6 @@ class Item < ApplicationRecord
     validates :image         
     validates :name             
     validates :introduction     
-    validates :category_id
-    validates :condition_id
-    validates :delivery_cost_id
-    validates :prefecture_id
-    validates :delivery_days_id
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     with_options numericality: { other_than: 1 } do
       validates :category_id 
