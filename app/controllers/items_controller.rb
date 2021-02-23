5bed1d34f41ecc209be03e69868f05646ce00d63
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-02-23 15:05:41 +0900

    コメントアウト復活

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create ]
 
+  def index
+    @items = Item.all.order(id: "DESC")
+  end
 
   # def new
   #   @item = Item.new
@@ -13,7 +16,11 @@ class ItemsController < ApplicationController
   #   else
   #     render :new
   #   end
-  # end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 
   private
 
