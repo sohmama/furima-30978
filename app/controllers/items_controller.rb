aedac96dc255b5daff1b7aed6763518346218e16
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-02-25 12:48:54 +0900

    コードレビューに基づく修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit ]
+  before_action :authenticate_user!, except: [:index, :show ]
   before_action :item_find, except: [:index, :new, :create]
   before_action :redirect_root, only: [:edit, :update, :destroy]
 
