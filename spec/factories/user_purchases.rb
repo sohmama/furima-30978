1553994b4c426e865570f50b5957599421d864d5
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-02-27 10:55:26 +0900

    条件分岐の実装など

--- spec/factories/user_purchases.rb
+++ spec/factories/user_purchases.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
   factory :user_purchase do
     token {"tok_abcdefghijk00000000000000000"}
+    user_id {1}
+    item_id {1}
     postal_code {'123-4567'}
     prefecture_id {2}
     municipality {'東京都中央区'}
     address {'銀座1-1-1'}
-    building {山野ビル402}
+    building {'山野ビル402'}
     tel {'09012345678'}
   end
 end
